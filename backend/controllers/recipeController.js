1ce6ca6ce823c10c2cd88b02959700386bc8ba37
Jahmella Williams <simmons.jahmella@gmail.com>
2022-10-15 21:56:52 -0400

    Updates controller

--- backend/controllers/recipeController.js
+++ backend/controllers/recipeController.js
@@ -1,35 +1,37 @@
+const asyncHandler = require('express-async-handler')
+
 //@desc Get recipes
 //@route GET /api/recipes
 //@access Private
-const getRecipes = (req, res) => {
+const getRecipes = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Get recipes'})
-}
+})
 
 //@desc Set recipe
 //@route POST /api/recipes
 //@access Private
-const setRecipes = (req, res) => {
+const setRecipes = asyncHandler(async (req, res) => {
     if(!req.body.text){
         res.status(400)
         throw new Error('Please add a text field')
     }
 
     res.status(200).json( {message: `Set favorite recipe`} )
-}
+})
 
 //@desc Update recipes
 //@route PUT /api/recipes/:id
 //@access Private
-const updateRecipes = (req, res) => {
+const updateRecipes = asyncHandler(async (req, res) => {
     res.status(200).json( {message: `Update favorite recipe ${req.params.id}`} )
-}
+})
 
 //@desc Delete goal
 //@route DELETE /api/goals/:id
 //@access Private
-const deleteRecipes = (req, res) => {
+const deleteRecipes = asyncHandler(async (req, res) => {
     res.status(200).json( {message: `Delete favorite ${req.params.id}`} )
-}
+})
 module.exports = {
     getRecipes, 
     setRecipes,
--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,10 +1,14 @@
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500
 
-    res.status(stausCode)
+    res.status(statusCode)
 
     res.json({
         message: err.message,
         stack: process.env.NODE_ENV === 'production' ? null : err.stack
     })
+}
+
+module.exports = {
+    errorHandler,
 }
