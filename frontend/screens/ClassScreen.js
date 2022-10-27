deb123f6bcd2a51a3f52ade177779c45695d1342
Jahmella Williams <simmons.jahmella@gmail.com>
2022-10-27 00:09:40 -0400

    Creates student

---
+++ frontend/screens/ClassScreen.js
@@ -0,0 +1,7 @@
+import { View, Text } from 'react-native'
+
+const ClassScreen = () => {
+    return <View><Text>This is the class screen</Text></View>
+}
+
+export default ClassScreen
--- frontend/screens/RosterScreen.js
+++ frontend/screens/RosterScreen.js
@@ -1,14 +1,36 @@
 
-import {useState } from 'react'
-import { View, StyleSheet, FlatList } from 'react-native'
-import { Button, ListItem } from '@rneui/themed'
-import StudentScreen from './StudentScreen'
+import { useState } from 'react'
+import { View, Text, StyleSheet, FlatList, Modal, Pressable, Alert } from 'react-native'
+import {  ListItem, Icon, Input } from '@rneui/themed'
+
+
 import { STUDENTS } from '../shared/students'
 
 
-const  RosterScreen = ({navigation}) => {
-    
-  const [students, setStudents ] = useState(STUDENTS)
+
+const  RosterScreen = ({navigation}) => {   
+
+const idNumber = () => {
+    return Math.floor(Math.random()*10000) + 1;
+
+}
+const [students, setStudents ] = useState(STUDENTS)
+const [modalVisible, setModalVisible] = useState(false);
+const [newStudentName, setNewStudentName] = useState("");
+
+const [newStudentBehavior, setNewStudentBehavior] = useState("Fair")
+const [newStudentAssignment, setNewStudentAssignment] = useState(false)
+
+const addNewStudent = () => {
+    const newStudent = { id: idNumber, name: newStudentName, behavior: newStudentBehavior, assignments: newStudentAssignment}
+    setStudents(students.push(newStudent))
+}
+
+
+ 
+const resetInput = () => {
+    setNewStudentName("")
+}
 
     const renderStudent = ({item: student}) => {
         return(
@@ -27,27 +49,64 @@ const  RosterScreen = ({navigation}) => {
         <>
           
         <View style={styles.container}>
-           
-                <Button 
-                containerStyle={{
-                    width: 200,
-                }}
-                title="Add Student"
-                type="clear"
-                color="steelblue"
-                icon={{type:'font-awesome', name: 'plus', color:'steelblue'}}
-                iconContainerStyle={{marginRight: 10}}
-                onPress={() => navigation.navigate('Student') }
+              
+            <FlatList
+                data={students}
+                renderItem={renderStudent}
+                keyExtractor={(item) => item.id.toString()}
                 />
-        
-        <FlatList
-            data={students}
-            renderItem={renderStudent}
-            keyExtractor={(item) => item.id.toString()}
-            />
+                
+            <Pressable
+            style={[styles.button]}
+            onPress={() => setModalVisible(true)}
+            >
             
+            <Text style={styles.textStyle}> <Icon 
+            type="font-awesome"
+            name="plus"
+            color="steelblue"
+            /> Add Student</Text>
+            </Pressable>
         </View>
-        
+      
+            <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+            Alert.alert("Cancel new student");
+            setModalVisible(!modalVisible);
+            }}
+        >
+            <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+            <Input 
+            placeholder="Student's Name"
+            value={newStudentName}
+            onChangeText={(value)  => setNewStudentName(value)}
+             />
+           
+
+                <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => {
+                
+                    Alert.alert(`${newStudentName} added`);
+                    
+                    addNewStudent()
+                    console.log(JSON.stringify(students))
+                    setModalVisible(!modalVisible);
+                    resetInput();
+                   
+                }     
+                }
+                >
+                <Text>Add new student</Text>
+                </Pressable>
+            </View>
+            </View>
+        </Modal>
+       
         </>
   
     )    
@@ -58,6 +117,46 @@ const styles = StyleSheet.create({
         flex: 1,
         margin: 20
     }, 
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+      
+        marginTop: 22
+      },
+      modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+      },
+      button: {
+        borderRadius: 20,
+        padding: 10,
+        
+      },
+      
+      buttonClose: {
+        backgroundColor: "steelblue",
+      },
+      textStyle: {
+        color: "steelblue",
+        fontWeight: "bold",
+        textAlign: "center",
+        fontSize: 20
+      },
+      modalText: {
+        marginBottom: 15,
+        textAlign: "center"
+      },
 
 })
 
