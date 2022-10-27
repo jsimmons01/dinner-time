7da549fcecaf3f7107e1c83eac45aa2fdcd97ffa
Jahmella Williams <simmons.jahmella@gmail.com>
2022-10-27 18:25:58 -0400

    Installs redux

--- frontend/screens/StudentScreen.js
+++ frontend/screens/StudentScreen.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Button, Icon } from '@rneui/themed'
 import RenderStudent from '../features/students/RenderStudent'
+import { Alert } from 'react-native';
 
 
-const StudentScreen = ({ route }) => {
+
+const StudentScreen = ({ route, }) => {
+
+ 
   const { student } = route.params;
 
   return (
     <>
      <RenderStudent student={student} />
-    <Button  type="clear" >
+    <Button  type="clear"
+            onPress= { () =>
+            Alert.alert(`Are you sure you want to delete: ${student}`)} >
       <Icon type="font-awesome"
             name="trash"
             color="steelblue"
